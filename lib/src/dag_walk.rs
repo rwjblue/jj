rwjblue/jj

commit d9c68e08b1a575e63443cacb130806536de06e8a

everything: Rename `branches` to `bookmarks`
Jujutsu's branches do not behave like Git branches, which is a major
hurdle for people adopting it from Git. They rather behave like
Mercurial's (hg) bookmarks. 

We've had multiple discussions about it in the last ~1.5 years about this rename in the Discord, 
where multiple people agreed that this _false_ familiarity does not help anyone. Initially we were 
reluctant to do it but overtime, more and more users agreed that `bookmark` was a better for name 
the current mechanism. This may be hard break for current `jj branch` users, but it will immensly 
help Jujutsu's future, by defining it as our first own term. The `[experimental-moving-branches]` 
config option is currently left alone, to force not another large config update for
users, since the last time this happened was when `jj log -T show` was removed, which immediately 
resulted in breaking users and introduced soft deprecations.

This name change will also make it easier to introduce Topics (#3402) as _topological branches_ 
with a easier model. 

This was mostly done via LSP, ripgrep and sed and a whole bunch of manual changes either from
me being lazy or thankfully pointed out by reviewers.

lib/src/dag_walk.rs
@@ -171,7 +171,7 @@ where
 /// For example, topological order of chronological data should respect
 /// timestamp (except a few outliers caused by clock skew.)
 ///
-/// Use `topo_order_reverse()` if the DAG is heavily branched. This can
+/// Use `topo_order_reverse()` if the DAG is heavily bookmarked. This can
 /// only process linear part lazily.
 pub fn topo_order_reverse_lazy<T, ID, II, NI>(
     start: II,
@@ -277,7 +277,7 @@ impl<T: Ord, ID: Hash + Eq + Clone, E> TopoOrderReverseLazyInner<T, ID, E> {
     }
 }
 
-/// Splits DAG at single fork point, and extracts branchy part as sub graph.
+/// Splits DAG at single fork point, and extracts bookmarky part as sub graph.
 ///
 /// ```text
 ///  o | C
@@ -286,9 +286,9 @@ impl<T: Ord, ID: Hash + Eq + Clone, E> TopoOrderReverseLazyInner<T, ID, E> {
 ///  o A
 /// ```
 ///
-/// If a branch reached to root (empty neighbors), the graph can't be split
-/// anymore because the other branch may be connected to a descendant of
-/// the rooted branch.
+/// If a bookmark reached to root (empty neighbors), the graph can't be split
+/// anymore because the other bookmark may be connected to a descendant of
+/// the rooted bookmark.
 ///
 /// ```text
 ///  o | C
@@ -809,7 +809,7 @@ mod tests {
         assert_eq!(common, vec!['E', 'D', 'C', 'B', 'a']);
 
         // The root node 'a' is visited before 'C'. If the graph were split there,
-        // the branch 'C->B->a' would be orphaned.
+        // the bookmark 'C->B->a' would be orphaned.
         let common = topo_order_reverse_lazy(vec!['E'], id_fn, neighbors_fn).collect_vec();
         assert_eq!(common, vec!['E', 'D', 'C', 'B', 'a']);
 
@@ -1067,7 +1067,7 @@ mod tests {
     }
 
     #[test]
-    fn test_topo_order_reverse_cycle_to_branchy_sub_graph() {
+    fn test_topo_order_reverse_cycle_to_bookmarky_sub_graph() {
         // This graph:
         //  o D
         //  |\

commit 8469cc0995729c391f9edb8253bb67c7020f0b1e

lib: dag_walk: fix doc comment typos
This commit fixes more typos unintentionally introduced in d9c68e08,
when renaming `jj branch` to `jj bookmark`.

lib/src/dag_walk.rs
@@ -171,7 +171,7 @@ where
 /// For example, topological order of chronological data should respect
 /// timestamp (except a few outliers caused by clock skew.)
 ///
-/// Use `topo_order_reverse()` if the DAG is heavily bookmarked. This can
+/// Use `topo_order_reverse()` if the DAG is heavily branched. This can
 /// only process linear part lazily.
 pub fn topo_order_reverse_lazy<T, ID, II, NI>(
     start: II,
@@ -286,9 +286,9 @@ impl<T: Ord, ID: Hash + Eq + Clone, E> TopoOrderReverseLazyInner<T, ID, E> {
 ///  o A
 /// ```
 ///
-/// If a bookmark reached to root (empty neighbors), the graph can't be split
-/// anymore because the other bookmark may be connected to a descendant of
-/// the rooted bookmark.
+/// If a branch reached to root (empty neighbors), the graph can't be split
+/// anymore because the other branch may be connected to a descendant of
+/// the rooted branch.
 ///
 /// ```text
 ///  o | C
@@ -809,7 +809,7 @@ mod tests {
         assert_eq!(common, vec!['E', 'D', 'C', 'B', 'a']);
 
         // The root node 'a' is visited before 'C'. If the graph were split there,
-        // the bookmark 'C->B->a' would be orphaned.
+        // the branch 'C->B->a' would be orphaned.
         let common = topo_order_reverse_lazy(vec!['E'], id_fn, neighbors_fn).collect_vec();
         assert_eq!(common, vec!['E', 'D', 'C', 'B', 'a']);
 